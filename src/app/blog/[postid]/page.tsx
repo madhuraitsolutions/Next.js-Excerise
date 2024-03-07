032f0d695e722dd75f0c54c8099236386419517f
Kombiah <kombiahrk@yahoo.com>
2024-03-07 23:11:24 +0530

    Dynamically Generate Metadata Example

--- src/app/blog/[postid]/page.tsx
+++ src/app/blog/[postid]/page.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Metadata } from 'next'
 
-export default function page({params}:{
-    params:{postid:string}
-}) {
+type Props = {
+  params : {
+    postid : string
+  }
+}
+
+export const generateMetadata = ({params}: Props): Metadata => {
+    return {
+      title : `Blog Post ID - ${params.postid}`
+    }
+}
+
+export default function page({params}: Props) {
   return (
-    <div>Post Details of ID - {params.postid}</div>
+    <div className='h-screen bg-yellow-200'>
+      <h1 className='p-4 pt-20 text-8xl font-extralight text-center'>Post Details of ID - {params.postid}</h1>
+    </div>
   )
 }
