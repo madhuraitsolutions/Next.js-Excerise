9bef3b10f9657a0778d98fda6b62c89d97bff43e
Kombiah <kombiahrk@yahoo.com>
2024-03-07 23:28:57 +0530

    Example of Title Metadata (String and Object)

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+export const metadata = {
+  title : {
+    absolute:'This is Aboslute Example'
+  }
+}
+
 export default function page() {
   return (
-    <div>
+    <div className='h-screen bg-slate-200 text-center'>
         <h1>Blog Post 1</h1>
         <h1>Blog Post 2</h1>
         <h1>Blog Post 3</h1>
