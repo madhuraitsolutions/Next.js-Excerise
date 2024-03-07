adeec834680198d4af84555bce3fac8cefd1d90f
Kombiah <kombiahrk@yahoo.com>
2024-03-07 14:21:11 +0530

    Profile page.tsx Updated

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div><h1 className="text-9xl">Profile Page</h1></div>
+    <div>
+        <h1 className="text-9xl">Profile Page</h1>
+    </div>
   )
 }
