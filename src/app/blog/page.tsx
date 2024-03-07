a77f2fabaa963e9b2586f8129a3e336de11fae84
Kombiah <kombiahrk@yahoo.com>
2024-03-07 15:16:01 +0530

    Blog page.tsx Updated

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h1>Blog Post 1</h1>
+        <h1>Blog Post 2</h1>
+        <h1>Blog Post 3</h1>
+        <h1>Blog Post 4</h1>
+        <h1>Blog Post 5</h1>
+    </div>
+  )
+}
