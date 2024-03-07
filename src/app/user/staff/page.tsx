9067c1737d019b584eda4a62a9fe1c39e25a2629
Kombiah <kombiahrk@yahoo.com>
2024-03-07 22:33:05 +0530

    Staff Page.tsx updated

--- src/app/user/staff/page.tsx
+++ src/app/user/staff/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
+    <div className='h-screen bg-yellow-200'>
         <h1 className='p-4 pt-20 text-8xl font-extralight text-center'>Staff Dashboard</h1>
     </div>
   )
