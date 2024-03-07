fa9027383ed864085b56f680deddddc7cd74733d
Kombiah <kombiahrk@yahoo.com>
2024-03-07 15:53:31 +0530

    custom 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function notfound() {
+  return (
+    <div>
+        <h1 className='font-bold'>Page Not Found</h1>
+        <h2>The requested resource could not be found</h2>
+    </div>
+  )
+}
