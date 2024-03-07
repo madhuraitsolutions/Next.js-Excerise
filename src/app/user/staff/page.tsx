67db8435c5c71d4d813f17ab8353da4d4518ad03
Kombiah <kombiahrk@yahoo.com>
2024-03-07 20:23:02 +0530

    Staff Page.tsx added in User folder

---
+++ src/app/user/staff/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h1 className='p-4 pt-20 text-8xl font-extralight text-center'>Staff Dashboard</h1>
+    </div>
+  )
+}
