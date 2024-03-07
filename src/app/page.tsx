27ab715cbaa0464872df719fbbf88a54d7b87cdc
Kombiah <kombiahrk@yahoo.com>
2024-03-07 22:54:57 +0530

    Home Page.tsx updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-blue-200 h-screen">
+      <h3 className='p-4 pt-20 text-8xl font-extralight text-center'>Home Page</h3>
+    </div>
   );
 }
