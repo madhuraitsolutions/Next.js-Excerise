8492fc2e0be9ae19658bb4809d04580d6373f1e9
Kombiah <kombiahrk@yahoo.com>
2024-03-08 15:16:41 +0530

    (auth)/layout.tsx file updated

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navlinks = [
     { name: 'Register', href: '/register' },
@@ -16,9 +17,15 @@ export default function RootLayout({
 }) {
 
     const pathname = usePathname()
+    const [input, setInput] = useState('')
 
     return (
         <>
+            <div className="p-4">
+                <input value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    className="outline rounded-sm" />
+            </div>
             {
                 navlinks.map((link) => {
                     const isActive = pathname.startsWith(link.href)
