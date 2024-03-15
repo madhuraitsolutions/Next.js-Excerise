4bc1c5a47a4fd84998a16dc86b929c25cbab3b54
Kombiah <kombiahrk@yahoo.com>
2024-03-15 14:36:36 +0530

    Loading Page Example

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='h-screen bg-yellow-300'>
+        <h1 className='text-8xl text-blue-400 text-center'>
+            Loading...
+        </h1>
+    </div>
+  )
+}
